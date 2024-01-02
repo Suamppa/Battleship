7fb000e57fa279ea1f55ead4a4ddf2da1a6b5e4e
Suamppa <18483942+Suamppa@users.noreply.github.com>
2024-01-02 21:47:51 +0200

    GridManager now works on the parent TileMap rather than the individual layers

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,28 +6,33 @@ public class GridManager : MonoBehaviour {
     public int boardSizeX;
     public int boardSizeY;
     // Set the tile to be used
-    public TileBase tile;
+    // public TileBase tile;
     // Optional reference to a camera to set to the center of the board
     public Transform sceneCamera;
 
     // Reference to the tilemap
-    private Tilemap tilemap;
+    // private Tilemap tilemap;
 
     private void Start() {
-        tilemap = GetComponent<Tilemap>();
-        GenerateBoard();
+        // tilemap = GetComponent<Tilemap>();
+        Tilemap[] layers = GetComponentsInChildren<Tilemap>();
+        foreach (Tilemap layer in layers) {
+            TileBase tile = layer.GetTile(new Vector3Int(0, 0, 0));
+            Debug.Log("Drawing layer " + layer.name + " with tile " + tile.name);
+            GenerateBoard(layer, tile);
+        }
+
+        if (sceneCamera != null) {
+            sceneCamera.position = new Vector3(boardSizeX / 2f, boardSizeY / 2f, -10);
+        }
     }
 
-    private void GenerateBoard() {
+    private void GenerateBoard(Tilemap tilemap, TileBase tile) {
         for (int x = 0; x < boardSizeX; x++) {
             for (int y = 0; y < boardSizeY; y++) {
                 Vector3Int tilePosition = new(x, y, 0);
                 tilemap.SetTile(tilePosition, tile);
             }
-        }
-
-        if (sceneCamera != null) {
-            sceneCamera.position = new Vector3(boardSizeX / 2f, boardSizeY / 2f, -10);
         }
     }
 }
