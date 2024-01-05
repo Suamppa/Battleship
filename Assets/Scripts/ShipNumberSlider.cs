73258982ba845fa8b8f7d72b97888874e18aff4e
Suamppa <18483942+Suamppa@users.noreply.github.com>
2024-01-06 00:50:30 +0200

    Ship number slider value now updates

--- Assets/Scripts/ShipNumberSlider.cs
+++ Assets/Scripts/ShipNumberSlider.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipNumberSlider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Slider slider;
+    private TextMeshProUGUI handleText;
+
+    private void Start() {
+        slider = GetComponent<Slider>();
+        handleText = GetComponentInChildren<TextMeshProUGUI>();
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnSliderValueChanged(float value) {
+        handleText.text = value.ToString();
     }
 }
