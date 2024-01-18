e1a8333662823ff3840280a12b1ba165e9e8acd1
Suamppa <18483942+Suamppa@users.noreply.github.com>
2024-01-18 22:34:42 +0200

    Add start button to ship selection screen context

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -3,12 +3,12 @@ using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour {
     private Tilemap[] layers;
+    // The size of the board
+    private int boardSizeX, boardSizeY;
 
+    public GameOptionsProvider gameOptions;
     // The grid of the game board
     public int[,] GameBoard { get; private set; }
-    // Set the size of the board
-    public int boardSizeX;
-    public int boardSizeY;
     // Optional reference to a camera to set to the center of the board
     public Transform sceneCamera;
 
@@ -20,16 +20,13 @@ public class GridManager : MonoBehaviour {
     }
 
     private void Start() {
-        InitializeBoard();
-        foreach (Tilemap layer in layers) {
-            TileBase tile = layer.GetTile(new Vector3Int(0, 0, 0));
-            Debug.Log("Drawing layer " + layer.name + " with tile " + tile.name);
-            DrawBoard(layer, tile);
-        }
+        UpdateBoardSize();
+        ClearBoard();
+    }
 
-        if (sceneCamera != null) {
-            sceneCamera.position = new Vector3(boardSizeX / 2f, boardSizeY / 2f, -10);
-        }
+    private void UpdateBoardSize() {
+        boardSizeX = gameOptions.BoardWidth;
+        boardSizeY = gameOptions.BoardHeight;
     }
 
     private void InitializeBoard() {
@@ -41,12 +38,42 @@ public class GridManager : MonoBehaviour {
         }
     }
 
-    private void DrawBoard(Tilemap tilemap, TileBase tile) {
+    private void ClearLayer(Tilemap tilemap) {
+        tilemap.ClearAllTiles();
+    }
+
+    private void DrawLayer(Tilemap tilemap, TileBase tile) {
         for (int x = 0; x < boardSizeX; x++) {
             for (int y = 0; y < boardSizeY; y++) {
                 Vector3Int tilePosition = new(x, y, 0);
                 tilemap.SetTile(tilePosition, tile);
             }
+        }
+    }
+
+    public void DrawBoard() {
+        ClearBoard();
+        UpdateBoardSize();
+        InitializeBoard();
+        foreach (Tilemap layer in layers) {
+            TileBase tile = layer.GetTile(new Vector3Int(0, 0, 0));
+            Debug.Log("Drawing layer " + layer.name + " with tile " + tile.name);
+            DrawLayer(layer, tile);
+        }
+
+        AlignCamera();
+    }
+
+    public void ClearBoard() {
+        foreach (Tilemap layer in layers) {
+            ClearLayer(layer);
+        }
+    }
+
+    public void AlignCamera(float cameraOffsetX = 0f, float cameraOffsetY = 0f) {
+        if (sceneCamera != null) {
+            Vector3 newPosition = new((boardSizeX / 2f) + cameraOffsetX, (boardSizeY / 2f) + cameraOffsetY, sceneCamera.position.z);
+            sceneCamera.position = newPosition;
         }
     }
 }
---
+++ Assets/Scripts/ManageStartButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ManageStartButton : MonoBehaviour
+{
+    public GameObject startButton;
+
+    private void Start() {
+        startButton.SetActive(false);
+    }
+
+    private void OnEnable() {
+        startButton.SetActive(true);
+    }
+
+    private void OnDisable() {
+        startButton.SetActive(false);
+    }
+}
